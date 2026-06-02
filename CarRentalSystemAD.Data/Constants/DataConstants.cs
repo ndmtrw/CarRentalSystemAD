99c6020fc028ef4f30286c01636ae19b5ab937e7
Atanas Dimitrov <atanas.a.dimitrov.highschool@buditel.bg>
2026-06-02 09:57:55 +0300

    Add application db context and seed data

---
+++ CarRentalSystemAD.Data/Constants/DataConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRentalSystemAD.Data.Constants;
+
+public static class DataConstants
+{
+    public const int CarBrandMaxLength = 80;
+    public const int CarModelMaxLength = 80;
+
+    public const int CategoryNameMaxLength = 60;
+
+    public const int UserFirstNameMaxLength = 50;
+    public const int UserLastNameMaxLength = 50;
+}
