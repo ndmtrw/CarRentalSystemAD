f15c54652c7ec05339fcc7a26be6f0f7fdaae6f9
ndmtrw <atanas.a.dimitrov.highschool@buditel.bg>
2026-06-07 23:30:17 +0300

    Add admin area dashboard

---
+++ CarRentalSystemAD.Web/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRentalSystemAD.Web.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = "Administrator")]
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
