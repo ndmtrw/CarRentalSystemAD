20973f63dcc3973b8f5363474381c3930f41e31f
Atanas Dimitrov <atanas.a.dimitrov.highschool@buditel.bg>
2026-06-08 10:45:00 +0300

    Add category management

--- CarRentalSystemAD.Services/Contracts/ICategoryService.cs
+++ CarRentalSystemAD.Services/Contracts/ICategoryService.cs
@@ -1,6 +1,18 @@
-﻿namespace CarRentalSystemAD.Services.Contracts;
+﻿using CarRentalSystemAD.Services.Models;
+
+namespace CarRentalSystemAD.Services.Contracts;
 
 public interface ICategoryService
 {
-    Task<IEnumerable<string>> GetAllNamesAsync();
+    Task<IEnumerable<CategoryViewModel>> GetAllAsync();
+
+    Task<CategoryFormModel?> GetByIdAsync(int id);
+
+    Task AddAsync(CategoryFormModel model);
+
+    Task EditAsync(int id, CategoryFormModel model);
+
+    Task DeleteAsync(int id);
+
+    Task<bool> ExistsAsync(int id);
 }
--- CarRentalSystemAD.Services/Services/CategoryService.cs
+++ CarRentalSystemAD.Services/Services/CategoryService.cs
@@ -1,5 +1,7 @@
 ﻿using CarRentalSystemAD.Data.Data;
+using CarRentalSystemAD.Data.Models;
 using CarRentalSystemAD.Services.Contracts;
+using CarRentalSystemAD.Services.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace CarRentalSystemAD.Services.Services;
@@ -13,10 +15,69 @@ public class CategoryService : ICategoryService
         this.context = context;
     }
 
-    public async Task<IEnumerable<string>> GetAllNamesAsync()
+    public async Task<IEnumerable<CategoryViewModel>> GetAllAsync()
     {
         return await context.Categories
-            .Select(c => c.Name)
+            .Select(c => new CategoryViewModel
+            {
+                Id = c.Id,
+                Name = c.Name
+            })
             .ToListAsync();
+    }
+
+    public async Task<CategoryFormModel?> GetByIdAsync(int id)
+    {
+        return await context.Categories
+            .Where(c => c.Id == id)
+            .Select(c => new CategoryFormModel
+            {
+                Name = c.Name
+            })
+            .FirstOrDefaultAsync();
+    }
+
+    public async Task AddAsync(CategoryFormModel model)
+    {
+        var category = new Category
+        {
+            Name = model.Name
+        };
+
+        await context.Categories.AddAsync(category);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task EditAsync(int id, CategoryFormModel model)
+    {
+        var category = await context.Categories.FindAsync(id);
+
+        if (category == null)
+        {
+            return;
+        }
+
+        category.Name = model.Name;
+
+        await context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        var category = await context.Categories.FindAsync(id);
+
+        if (category == null)
+        {
+            return;
+        }
+
+        context.Categories.Remove(category);
+
+        await context.SaveChangesAsync();
+    }
+
+    public async Task<bool> ExistsAsync(int id)
+    {
+        return await context.Categories.AnyAsync(c => c.Id == id);
     }
 }
